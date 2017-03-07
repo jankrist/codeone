07f80990c3d41740ff9ab0b44e4e05cdcb08c757
Jan Krist <jan.krist@eman.cz>
2017-03-07 10:05:24 +0100

    initial commit

---
+++ app/src/main/java/cz/jankrist/codeone/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package cz.jankrist.codeone.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cz.jankrist.codeone.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
